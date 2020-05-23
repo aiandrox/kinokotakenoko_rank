77f4cc805ee53f18609ac2b19ea5efdcf30ad96b
Aiandrox <aiandrox7@gmail.com>
2020-05-23 10:55:06 +0900

    [Setup] VuefireでFirestoreの値を取得

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import { firestorePlugin } from "vuefire";
+import firebase from "firebase";
+import "firebase/firestore";
+
+Vue.use(firestorePlugin);
+firebase.initializeApp({
+  apiKey: "AIzaSyBRIBy50JxV3l7utT5m7wSP4E-UcxDNPwI",
+  authDomain: "like-ranking.firebaseapp.com",
+  databaseURL: "https://like-ranking.firebaseio.com",
+  projectId: "like-ranking",
+  storageBucket: "like-ranking.appspot.com",
+  messagingSenderId: "202579502101",
+  appId: "1:202579502101:web:ee52527232d73a9dd01a1c",
+  measurementId: "G-145DJJ1YYT",
+});
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1>きのこ派 vs たけのこ派</h1>
+    <h1>{{ rankings[0].left_name }} vs {{ rankings[0].right_name }}</h1>
+    {{ rankings[0] }}
     <!-- 総得票数 -->
+    総得票数{{ totalVote }}
     <!-- 帯グラフ -->
     <the-graph />
     <!-- きのこ得票数、投票ボタン v-forかな -->
@@ -11,12 +13,29 @@
 </template>
 
 <script>
+import { db } from "@/main";
 import theGraph from "../components/TheGraph";
 
 export default {
   name: "Top",
   components: {
     theGraph,
+  },
+  data() {
+    return {
+      rankings: [],
+    };
+  },
+  computed: {
+    totalVote() {
+      return this.rankings[0].right_vote + this.rankings[0].left_vote;
+    },
+  },
+  firestore() {
+    return {
+      // firestoreのcommentsコレクションを参照
+      rankings: db.collection("rankings"),
+    };
   },
 };
 </script>
