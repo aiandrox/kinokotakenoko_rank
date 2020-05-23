e44bf9a6be8785c8a06fa95d91fb964304e084ad
Aiandrox <aiandrox7@gmail.com>
2020-05-23 15:33:02 +0900

    [Design] 基本のデザイン完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    <the-top />
+    <div id="wrapper">
+      <the-top />
+    </div>
   </div>
 </template>
 
 <script>
 import theTop from "./pages/Top";
+
 export default {
   name: "App",
   components: {
@@ -20,8 +23,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
+  color: #1f1400;
+  margin: 0 auto;
+  width: 700px;
 }
 h3 {
   margin: 40px 0 0;
--- src/components/TheGraph.vue
+++ src/components/TheGraph.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
-    グラフ
-    <!-- たけのこ、きのこ -->
-    {{ ranking }}
-    <!-- 帯グラフ -->
+    <div class="graph-wrapper">
+      <div class="graph-body left" :style="{ width: leftPer + '%' }">
+        <div class="per">
+          <span>{{ leftPerRound }}</span
+          >%
+        </div>
+      </div>
+      <div class="graph-body right" :style="{ width: 100 - leftPer + '%' }">
+        <div class="per">
+          <span>{{ 100 - leftPerRound }}</span
+          >%
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,11 +21,47 @@
 export default {
   props: {
     ranking: {
-      right_name: "",
-      right_vote: 0,
-      left_name: "",
-      left_vote: 0,
+      rightName: "",
+      rightCount: 0,
+      leftName: "",
+      leftCount: 0,
+    },
+  },
+  computed: {
+    leftPer() {
+      return (
+        (this.ranking.leftCount * 100) /
+        (this.ranking.leftCount + this.ranking.rightCount)
+      );
+    },
+    leftPerRound() {
+      return Math.round(this.leftPer * 10) / 10;
     },
   },
 };
 </script>
+<style scoped>
+.graph-wrapper {
+  margin-top: 0.8rem;
+  font-family: "Prata", serif;
+  display: flex;
+  width: 100%;
+  height: 70px;
+}
+.graph-body {
+  height: 100%;
+  padding: 0 10px;
+}
+.graph-body span {
+  font-size: 200%;
+  font-weight: bolder;
+}
+.left {
+  text-align: left;
+  background-color: #ffe53b;
+}
+.right {
+  text-align: right;
+  background-color: #d1f558;
+}
+</style>
